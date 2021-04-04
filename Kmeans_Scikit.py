a38b30e324a4214db36c21747d5bfb912fab60b7
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-04-04 19:24:57 +0100

    another bit

--- Kmeans_Scikit.py
+++ Kmeans_Scikit.py
@@ -25,7 +25,8 @@ cancer=load_breast_cancer()
 # Familiarizing with Data .
 print(cancer)
 
-# Descriptive Analysis
+# General Descriptive Analysis : Understanding Data
+
 
 
 # In case the data is too disperse , consider Scaling it
@@ -37,6 +38,48 @@ print(y)
 
 # Step3 : Sampling the test and training subsets
 X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.2)
+
+# Step4 : Determining the Number of Clusters Using the " Elbow Method"
+from scipy.spatial.distance import cdist, pdist
+
+# Pick your k range , e.g K=10
+k_range = range(1,10)
+
+# Fit the kmeans model for each n_clusters = k
+# model = KMeans()
+# model.fit(X_train)
+k_means_var = [KMeans(n_clusters=k).fit(X_train) for k in k_range]
+
+# Fetch out and save the cluster centers for each model in an array
+centroids = [X.cluster_centers_ for X in k_means_var]
+
+# Calculate the Euclidean distance from each point to its assigned cluster's center
+k_eucld = [cdist(X_train, ctr, 'euclidean') for ctr in centroids]
+dst = [np.min(ke,axis=1) for ke in k_eucld]
+
+# Total within-cluster sum of squares
+twcss = [sum(d**2) for d in dst]
+
+# The total sum of squares
+tss = sum(pdist(X_train)**2)/X_train.shape[0]
+
+# The between-cluster sum of squares
+bcss = tss - twcss
+
+# Visualizing the elbow curve
+grph = plt.figure().add_subplot(111)
+grph.plot(k_range, bcss/tss*100, 'b*-')
+grph.set_ylim((0,100))
+plt.grid(True)
+plt.xlabel('The Number of Clusters ')
+plt.ylabel('The Percentage of variance explained')
+plt.title('Variance Explained per k')
+plt.show()
+
+
+
+
+
 
 
 
